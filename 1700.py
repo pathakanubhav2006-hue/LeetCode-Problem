1780687e243704ab9ec365ebfd6f7b45e491e215
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-07-15 10:22:24 +0530

    Improve code formatting and add docstring to solution 1700

--- 1700.py
+++ 1700.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def countStudents(self, students, sandwiches):
-        d={0:0,1:0}
+        """
+        Count how many students won't be able to eat sandwiches.
+        Uses a frequency map to track students by preference (0 or 1).
+        """
+        d = {0: 0, 1: 0}
         for i in students:
-            d[i]+=1
+            d[i] += 1
         
         for i in range(len(sandwiches)):
-            if d[sandwiches[i]]==0:
-                return (len(sandwiches)-i)
-
-            d[sandwiches[i]]=-1
-
-
+            if d[sandwiches[i]] == 0:
+                return len(sandwiches) - i
+            d[sandwiches[i]] -= 1
         
-        
-
-
-        
+        return 0
