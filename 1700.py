92f2c5fd40dfabba323a119597bed8a263d354c4
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-07-15 10:19:36 +0530

    Add LeetCode solution 1700

---
+++ 1700.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        d={0:0,1:0}
+        for i in students:
+            d[i]+=1
+        
+        for i in range(len(sandwiches)):
+            if d[sandwiches[i]]==0:
+                return (len(sandwiches)-i)
+
+            d[sandwiches[i]]=-1
+        return 0
+
+
+        
+        
+
+
+        
