707a141c9e6b25c3e52c6e7271151b32593bd3bf
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-07-28 23:01:56 +0530

    Time: 12 ms (70.32%) | Memory: 13.3 MB (77.59%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findErrorNums(self, nums):
+        n = len(nums)
+        count = [0] * (n + 1)
+        for x in nums:
+            count[x] += 1
+
+        duplicate = 0
+        missing = 0
+        for i in range(1, n + 1):
+            if count[i] == 2:
+                duplicate = i
+            elif count[i] == 0:
+                missing = i
+
+        return [duplicate, missing]
