2fa7da8d371620b7d8fde01e7bc252e4c3f509d6
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-07-31 12:24:58 +0530

    Time: 11 ms (53.31%) | Memory: 12.8 MB (6.44%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,11 +1,16 @@
 class Solution(object):
     def findRestaurant(self, list1, list2):
+        d = {}
+
+        for i in range(len(list2)):
+            d[list2[i]] = i
+
+        min1 =len(list1)+len(list2)
         Y = []
-        min1 = len(list1) + len(list2)
 
         for i in range(len(list1)):
-            if list1[i] in list2:
-                idx = list2.index(list1[i])
+            if list1[i] in d:
+                idx = d[list1[i]]
 
                 if i + idx < min1:
                     min1 = i + idx
