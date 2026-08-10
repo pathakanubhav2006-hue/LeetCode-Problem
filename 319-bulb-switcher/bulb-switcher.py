1805989a92a692d66b4def6c00b0bf2f79810837
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-08-10 14:11:35 +0530

    Time: 1 ms (4.98%) | Memory: 12.3 MB (54.98%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def bulbSwitch(self, n):
+        count=0
+        i=1
+        while(i*i<=n):
+            count+=1
+            i+=1
+        
+
+        return count
+        
