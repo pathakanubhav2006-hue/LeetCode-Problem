6116cc6f8e3d96c6ae1aadba5f02711dc3f9cff9
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-08-26 11:06:45 +0530

    Time: 819 ms (54.32%) | Memory: 24.3 MB (59.32%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def stoneGameVIII(self, stones):
+        n = len(stones)
+        prefix = [0] * n
+        prefix[0] = stones[0]
+        for i in range(1, n):
+            prefix[i] = prefix[i-1] + stones[i]
+        
+        best = prefix[n-1]
+        for i in range(n-2, 0, -1):
+            best = max(best, prefix[i] - best)
+        
+        return best
