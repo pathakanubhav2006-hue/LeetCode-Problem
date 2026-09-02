3904a82e59957210b84c0b61517bbe1e02abf3b6
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-09-02 12:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (22.50%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -1,19 +1,5 @@
 class Solution(object):
     def uniformArray(self, nums1):
-        odd = 0
-        even = 0
-
-        for x in nums1:
-            if x % 2:
-                odd += 1
-            else:
-                even += 1
-        if odd >= 2 and even >= 2:
-            return True
-        if odd > 0 and even > 0:
-            return True
-
-        if odd == len(nums1) or even == len(nums1):
-            return True
-
-        return False
+        return True
+        
+        
