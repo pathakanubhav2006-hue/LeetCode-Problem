11923122cfa8f4098cfb79e2c7d68a02446ebda8
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-09-16 14:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (38.60%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,6 +1,32 @@
 class Solution(object):
     def search(self, nums, target):
-        if target in nums:
-            return True
+        #if target in nums:
+        #    return True
+        #return False
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return True
+
+            if nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -= 1
+
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
         return False
         
