5d9cd0a88157ac8b0a0127e71004a5d3e9d953ef
Anubhav Pathak <pathakanubhav2006@gmail.com>
2026-09-16 14:52:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.99%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -1,18 +1,20 @@
 class Solution(object):
     def isUgly(self, n):
-        if n <= 0:
+        def check(n):
+            if n <= 0:
+                return False
+            if n == 1:
+                return True
+
+            if n % 2 == 0:
+                return check(n // 2)
+
+            if n % 3 == 0:
+                return check(n // 3)
+
+            if n % 5 == 0:
+                return check(n // 5)
+
             return False
-        
-        if n == 1:
-            return True
-        
-        if n % 2 == 0:
-            return self.isUgly(n // 2)
-        
-        if n % 3 == 0:
-            return self.isUgly(n // 3)
-        
-        if n % 5 == 0:
-            return self.isUgly(n // 5)
-        
-        return False
+
+        return check(n)
